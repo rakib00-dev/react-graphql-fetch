d8cca3216ace0818c97452db2ad8a8e1a1b2b2c2
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-06-14 10:37:53 +0600

    successfuly added the graphql data to the ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
-import { useQuery, gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_LOCATIONS = gql`
+  query Query {
+    country(code: "BR") {
+      name
+      native
+      capital
+      emoji
+      currency
+      languages {
+        code
+        name
+      }
+    }
+  }
+`;
+
+// interface dataType {
+//   capital: string;
+//   name: string;
+//   description: string;
+//   photo: string;
+// }
 
 function App() {
-  return (
-    <>
-      <h1>React + GraphQL API Fetch - Apollo</h1>
-    </>
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  console.log(data.country.languages);
+
+  return [data.country].map(
+    ({ capital, name, description, native, emoji, currency, languages }) => {
+      return (
+        <div key={name}>
+          <h1>{name}</h1>
+          <h2>{capital}</h2>
+          <p>{native}</p>
+          <p>{emoji}</p>
+          <p>{currency}</p>
+          <b>About this location:</b>
+          <p>{description}</p>
+          <p>
+            {'languages '}
+            {languages.map((e: any) => (
+              <>
+                <span key={e.name}>{e.name}</span>
+                <span key={e.code}>{e.code}</span>
+              </>
+            ))}
+          </p>
+          <br />
+        </div>
+      );
+    }
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
+  uri: 'https://countries.trevorblades.com/graphql',
   cache: new InMemoryCache(),
 });
 
